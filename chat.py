60bba5c060d0c308722ec4f831ed71259a89c6ea
Artem <f159752@gmail.com>
2018-10-07 21:34:45 +0300

    first commit

---
+++ chat.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, chatID="", field=[], cpt="", side="", level=-1, counter=-1):
+        self.chatID = chatID
+        self.field = field
+        self.cpt = cpt
+        self.side = side
+        self.level = level
+        self.counter = counter
---
+++ config.py
@@ -0,0 +1,3 @@
+#coding:utf-8
+
+TOKEN = "565969259:AAH_7skiQJklBq1gGYl3kN3LuyanNPIxVe0"
